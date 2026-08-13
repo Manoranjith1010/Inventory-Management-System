9ff3245f5791ee0a248633e88a68efaa56e422b8
Manoranjith <164288436+Manoranjith1010@users.noreply.github.com>
2026-08-13 01:06:00 +0000

    chore: remove legacy django project files

--- myproject/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django_mongoengine import mongo_admin
-from .models import Post
-
-class PostAdmin(mongo_admin.DocumentAdmin):
-    list_display = ('title', 'author')
-
-mongo_admin.site.register(Post, PostAdmin)
--- myproject/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import home, about
-
-urlpatterns = [
-    path('', home, name='home'),        # The empty string '' means the homepage
-    path('about/', about, name='about'),
-]
